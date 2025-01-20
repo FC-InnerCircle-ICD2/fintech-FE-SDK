37edfbf93f1ff908d0af3eedbba7a6cb9ec29441
HyoungMin <hyoungqu23@gmail.com>
2025-01-21 00:35:25 +0900

    refactor: type 선언을 분리한다

---
+++ src/entities/payment/types.ts
@@ -0,0 +1,20 @@
+import type { Response } from '../../shared/api/httpClient';
+
+export type Order = {
+  id: string;
+  amount: number;
+  orderName: string;
+};
+
+export type RedirectURL = {
+  successUrl: string;
+  failureUrl: string;
+};
+
+export type PaymentReq = Order & RedirectURL;
+export type PaymentRes = { token: string; expiredAt: string };
+
+export type PaymentApi = {
+  requestPayment: (body: PaymentReq) => Promise<Response<PaymentRes>>;
+  subscribePaymentEvents: (token: string) => EventSource;
+};
