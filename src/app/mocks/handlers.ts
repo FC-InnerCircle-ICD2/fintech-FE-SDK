f22f58af1697938653a813fcc0df5b5ce5b68a93
HyoungMin <hyoungqu23@gmail.com>
2025-01-21 09:21:06 +0900

    refactor: MSW handler의 subscribe payment events API에 에러 시나리오를 추가한다

--- src/app/mocks/handlers.ts
+++ src/app/mocks/handlers.ts
@@ -1,8 +1,68 @@
 import { http, HttpResponse } from 'msw';
+import type { PaymentReq, PaymentRes } from '../../entities/payment/types';
+import type { Response } from '../../shared/api/httpClient';
 import { API_ENDPOINTS } from '../../shared/config/apiEndpoints';
 import { DUMMY_API_CONFIG } from '../../shared/config/dummy';
-import type { Response } from '../../shared/api/httpClient';
-import type { PaymentReq, PaymentRes } from '../../entities/payment/types';
+
+export type EventName = 'method-verification' | 'payment-approval';
+
+export const SCENARIOS = {
+  SUCCESS: { code: 200, message: 'success' },
+  ERROR: {
+    TOKEN: { code: 401, message: 'unauthorized_token' },
+    PAYMENT_METHOD: {
+      PLAIN: { code: 500, message: 'payment_method_error' },
+      INVALID: { code: 400, message: 'invalid_payment_method' },
+    },
+    PAYMENT: {
+      PLAIN: { code: 500, message: 'payment_approval_error' },
+      INVALID: { code: 400, message: 'invalid_order_info' },
+      REJECT: { code: 403, message: 'reject_payment_approval' },
+    },
+  },
+} as const;
+
+type PaymentMethodRes = {
+  successURL: string;
+};
+
+type PaymentApprovalRes = {
+  paymentKey: string;
+};
+
+export type Event<T> = {
+  data: {
+    timestamp: string;
+    code: number;
+    data: T | null;
+    error: {
+      code: number;
+      message: string;
+    } | null;
+  };
+};
+
+const createEvent = <T>(
+  eventName: EventName,
+  code: number,
+  data: T | null = null,
+  error: { code: number; message: string } | null = null,
+) => {
+  const eventData: Event<T> = {
+    data: {
+      timestamp: new Date().toISOString(),
+      code,
+      data,
+      error,
+    },
+  };
+
+  return `event: ${eventName}\ndata: ${JSON.stringify(eventData)}\n\n`;
+};
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const successURL = `${DUMMY_API_CONFIG.ORDER.SUCCESS_URL}?amount=${DUMMY_API_CONFIG.ORDER.AMOUNT}&id=${DUMMY_API_CONFIG.ORDER.ID}`;
 
 export const handlers = [
   // request payment api
@@ -50,78 +110,199 @@ export const handlers = [
   ),
 
   // connect payment events api
-  http.get(API_ENDPOINTS.LISTEN_PAYMENT_EVENTS(':token'), ({ params }) => {
-    const { token } = params;
+  http.get(
+    API_ENDPOINTS.SUBSCRIBE_PAYMENT_EVENTS(':token'),
+    ({ params, request }) => {
+      const { token } = params;
+      const url = new URL(request.url);
+      const scenario = url.searchParams.get('scenario') || 'SUCCESS';
+      const delay = Number(url.searchParams.get('delay')) || 0;
 
-    console.log('👀 token', token);
+      const encoder = new TextEncoder();
+      const stream = new ReadableStream({
+        async start(controller) {
+          try {
+            if (delay) {
+              await sleep(delay);
+            }
 
-    const stream = new ReadableStream({
-      start(controller) {
-        const encoder = new TextEncoder();
+            // Unauthorized Token Scenario
+            if (token !== DUMMY_API_CONFIG.ORDER_TOKEN) {
+              controller.enqueue(
+                encoder.encode(
+                  createEvent<null>(
+                    'method-verification',
+                    SCENARIOS.ERROR.TOKEN.code,
+                    null,
+                    SCENARIOS.ERROR.TOKEN,
+                  ),
+                ),
+              );
+              controller.close();
+              return;
+            }
 
-        if (token !== DUMMY_API_CONFIG.ORDER_TOKEN) {
-          controller.enqueue(
-            encoder.encode(
-              `event: ${DUMMY_API_CONFIG.PAYMENT_EVENTS.RESULT}\ndata: ${JSON.stringify(
-                {
-                  type: DUMMY_API_CONFIG.PAYMENT_EVENTS.DATA.RESULT.FAILURE,
-                  data: 'Unauthorized',
-                },
-              )}\n\n`,
-            ),
-          );
+            switch (scenario) {
+              // case 'NETWORK_ERROR':
+              //   controller.enqueue(
+              //     encoder.encode(
+              //       createEvent<null>(
+              //         'method-verification',
+              //         STATUS.NETWORK_ERROR,
+              //         null,
+              //         {
+              //           code: STATUS.NETWORK_ERROR,
+              //           message: ERROR_MESSAGES.NETWORK_ERROR,
+              //         },
+              //       ),
+              //     ),
+              //   );
+              //   break;
 
-          return;
-        }
+              case 'PAYMENT_METHOD_ERROR':
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.ERROR.PAYMENT_METHOD.PLAIN.code,
+                      null,
+                      SCENARIOS.ERROR.PAYMENT_METHOD.PLAIN,
+                    ),
+                  ),
+                );
+                break;
 
-        // send event when payment method is ready
-        controller.enqueue(
-          encoder.encode(
-            `event: ${DUMMY_API_CONFIG.PAYMENT_EVENTS.METHOD}\ndata: ${JSON.stringify(
-              {
-                type: DUMMY_API_CONFIG.PAYMENT_EVENTS.METHOD,
-                data: DUMMY_API_CONFIG.PAYMENT_EVENTS.DATA.METHOD,
-              },
-            )}\n\n`,
-          ),
-        );
+              case 'INVALID_PAYMENT_METHOD':
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.ERROR.PAYMENT_METHOD.INVALID.code,
+                      null,
+                      SCENARIOS.ERROR.PAYMENT_METHOD.INVALID,
+                    ),
+                  ),
+                );
+                break;
 
-        // send event when payment succeed
-        setTimeout(() => {
-          controller.enqueue(
-            encoder.encode(
-              `event: ${DUMMY_API_CONFIG.PAYMENT_EVENTS.RESULT}\ndata: ${JSON.stringify(
-                {
-                  type: DUMMY_API_CONFIG.PAYMENT_EVENTS.DATA.RESULT.SUCCESS,
-                  data: 'Completed',
-                },
-              )}\n\n`,
-            ),
-          );
+              case 'PAYMENT_ERROR':
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.SUCCESS.code,
+                      { successURL },
+                      null,
+                    ),
+                  ),
+                );
 
-          controller.close();
-        }, 5000);
+                await sleep(1000);
 
-        // send event when payment failed
-        // setTimeout(() => {
-        //   controller.enqueue(
-        //     encoder.encode(
-        //       `event: ${DUMMY_API_CONFIG.PAYMENT_EVENTS.RESULT}\ndata: ${JSON.stringify(
-        //         {
-        //           type: DUMMY_API_CONFIG.PAYMENT_EVENTS.DATA.RESULT.FAILURE,
-        //           data: 'Failed',
-        //         },
-        //       )}\n\n`,
-        //     ),
-        //   );
-        // }, 5000);
-      },
-    });
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.ERROR.PAYMENT.PLAIN.code,
+                      null,
+                      SCENARIOS.ERROR.PAYMENT.PLAIN,
+                    ),
+                  ),
+                );
+                break;
 
-    return new HttpResponse(stream, {
-      headers: {
-        'Content-Type': 'text/event-stream',
-      },
-    });
-  }),
+              case 'INVALID_PAYMENT':
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.SUCCESS.code,
+                      { successURL },
+                      null,
+                    ),
+                  ),
+                );
+
+                await sleep(1000);
+
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.ERROR.PAYMENT.INVALID.code,
+                      null,
+                      SCENARIOS.ERROR.PAYMENT.INVALID,
+                    ),
+                  ),
+                );
+                break;
+
+              case 'REJECT_PAYMENT':
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.SUCCESS.code,
+                      { successURL },
+                      null,
+                    ),
+                  ),
+                );
+
+                await sleep(1000);
+
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.ERROR.PAYMENT.REJECT.code,
+                      null,
+                      SCENARIOS.ERROR.PAYMENT.REJECT,
+                    ),
+                  ),
+                );
+                break;
+
+              default:
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentMethodRes>(
+                      'method-verification',
+                      SCENARIOS.SUCCESS.code,
+                      { successURL },
+                      null,
+                    ),
+                  ),
+                );
+
+                await sleep(1000);
+
+                controller.enqueue(
+                  encoder.encode(
+                    createEvent<PaymentApprovalRes>(
+                      'payment-approval',
+                      SCENARIOS.SUCCESS.code,
+                      { paymentKey: 'abcdef' },
+                      null,
+                    ),
+                  ),
+                );
+            }
+
+            controller.close();
+          } catch (error) {
+            controller.error(error);
+          }
+        },
+      });
+
+      return new HttpResponse(stream, {
+        headers: {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          Connection: 'keep-alive',
+        },
+      });
+    },
+  ),
 ];
