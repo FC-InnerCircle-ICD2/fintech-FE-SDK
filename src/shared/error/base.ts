1c142fb87355030ab05ea8780526592f4d8deb6a
HyoungMin <hyoungqu23@gmail.com>
2025-01-25 00:00:08 +0900

    refactor: Base Error 관련 타입을 추가한다

--- src/shared/error/base.ts
+++ src/shared/error/base.ts
@@ -1,20 +1,12 @@
+import type { ErrorInfo } from './types';
+
 export class BaseError<T extends string> extends Error {
   name: T;
   message: string;
   code?: number;
   cause?: unknown;
 
-  constructor({
-    name,
-    message,
-    code,
-    cause,
-  }: {
-    name: T;
-    message: string;
-    code?: number;
-    cause?: unknown;
-  }) {
+  constructor({ name, message, code, cause }: ErrorInfo<T>) {
     super(message);
     this.name = name;
     this.message = message;
