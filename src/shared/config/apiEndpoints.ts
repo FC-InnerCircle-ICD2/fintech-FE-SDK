44e6f24b3224ef277dc5d09bf8c967ca78eabd1d
HyoungMin <hyoungqu23@gmail.com>
2025-02-04 21:39:43 +0900

    chore: vite.config에 proxy를 설정하고, API_BASE_URL을 업데이트한다

--- src/shared/config/apiEndpoints.ts
+++ src/shared/config/apiEndpoints.ts
@@ -1,8 +1,6 @@
 export const API_ENDPOINTS = {
-  REQUEST_PAYMENT: 'api/v1/payments',
-  SUBSCRIBE_PAYMENT_EVENTS: (orderToken: string) =>
-    `api/v1/payments/${orderToken}/events`,
+  REQUEST_PAYMENT: 'api/payment/v1/payments',
+  SUBSCRIBE_PAYMENT_EVENTS: 'api/payment/v1/sse/connect',
 } as const;
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, '') || '';
+export const API_BASE_URL = '/api';
