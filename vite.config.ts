8672b1c091a6e2c6655cf53f39f80b552ec1269a
HyoungMin <hyoungqu23@gmail.com>
2025-02-04 21:39:43 +0900

    chore: vite.config에 proxy를 설정하고, API_BASE_URL을 업데이트한다

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://payment.pay-200.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
