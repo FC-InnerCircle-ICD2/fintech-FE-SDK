98d1c827587885bb2891971d90b91ea206ea0d98
HyoungMin <hyoungqu23@gmail.com>
2025-02-07 08:22:12 +0900

    feat: `expiredAt`과 `Date.now()`를 비교하여, 해당 시간에 Callback 함수를 실행하는 `detectExpiredToken` 함수를 생성한다

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,2 +1,4 @@
+export { createSSE } from './createSSE';
 export { detectDevice } from './detectDevice';
+export { detectExpiredToken } from './detectExpiredToken';
 export { createShadowDOMRoot, openNewWindow } from './shadowDOM';
