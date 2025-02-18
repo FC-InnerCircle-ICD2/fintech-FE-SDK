93bfe40ecfbff76bfaf951e03f56d68eae441a97
HyoungMin <hyoungqu23@gmail.com>
2025-02-18 21:27:18 +0900

    refactor: `qrcode` 패키지를 동적으로 불러오는 `loadQRCode` 함수를 생성하고, Payment Window를 렌더할 때 동적으로 불러와 QR Code를 렌더한다

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -2,3 +2,5 @@ export { createSSE } from './createSSE';
 export { detectDevice } from './detectDevice';
 export { detectExpiredToken } from './detectExpiredToken';
 export { createShadowDOMRoot, openNewWindow } from './shadowDOM';
+export { loadQRCode } from './loadQRCode';
+export { compareServerTimestampByNow } from './compareTimestamp';
