1d5689a60d82a476cb7543091e106a37a08e07bb
HyoungMin <hyoungqu23@gmail.com>
2025-02-22 13:31:22 +0900

    fix: QR Code URL과 Next Button URL을 일치시킨다

--- src/shared/config/mobileAppPath.ts
+++ src/shared/config/mobileAppPath.ts
@@ -1,6 +1,2 @@
-export const MOBILE_APP_PATH = {
-  REDIRECT_URL: {
-    MOBILE: 'https://pay-200.vercel.app/payment/deeplink/success',
-    DESKTOP: 'https://pay-200.vercel.app/payment/detail',
-  },
-} as const;
+export const MOBILE_APP_PATH =
+  'https://pay-200.vercel.app/payment/deeplink/success' as const;
