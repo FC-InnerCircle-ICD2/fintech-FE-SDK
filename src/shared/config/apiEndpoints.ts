d190c03572df19765c0c1032169d3703640e96cd
HyoungMin <hyoungqu23@gmail.com>
2025-02-25 20:42:47 +0900

    chore: 배포를 위해 env 타입을 설정한다

--- src/shared/config/apiEndpoints.ts
+++ src/shared/config/apiEndpoints.ts
@@ -3,6 +3,7 @@ export const API_ENDPOINTS = {
   SUBSCRIBE_PAYMENT_EVENTS: '/api/v1/p/merchant/sse/connect',
 } as const;
 
-export const API_BASE_URL = import.meta.env.PROD
-  ? 'https://payment.pay-200.com'
-  : '/proxy';
+export const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://payment.pay-200.com'
+    : '/proxy';
