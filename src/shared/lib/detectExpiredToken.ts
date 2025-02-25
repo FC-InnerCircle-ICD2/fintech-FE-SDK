be68a8ac31b61b624dca3e9bf4910833fe1d4d24
HyoungMin <hyoungqu23@gmail.com>
2025-02-25 15:51:39 +0900

    fix: 이미 만료된 QR code의 경우, 바로 close 로직을 실행한다

--- src/shared/lib/detectExpiredToken.ts
+++ src/shared/lib/detectExpiredToken.ts
@@ -3,6 +3,11 @@ import { compareServerTimestampByNow } from './compareTimestamp';
 export const detectExpiredToken = (expiredAt: string, callback: () => void) => {
   const diff = compareServerTimestampByNow(expiredAt);
 
+  if (diff <= 0) {
+    callback();
+    return () => {};
+  }
+
   const timeoutId = setTimeout(callback, diff);
 
   return () => clearTimeout(timeoutId);
